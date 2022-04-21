0817db3cfb9815fa34fb900eada99887d4fb5dc0
ASC <asc@adamsmith.ie>
2022-04-21 20:01:38 +0100

    Added user auth for api access

--- app/tasks/views.py
+++ app/tasks/views.py
@@ -1,10 +1,15 @@
-import imp
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .serializers import CategorySerializer
 from .models import Category
 
 # Create your views here.
 class CategoryViewSet(viewsets.ModelViewSet):
+    permission_classes = [ permissions.IsAuthenticated ]
     serializer_class = CategorySerializer
-    queryset = Category.objects.all()
+    
+    def get_queryset(self):
+        return self.request.user.categories.all()
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
--- app/users/models.py
+++ app/users/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import UserManager
 from django.db.models import Q
+from django.core.validators import RegexValidator
+from django.utils.translation import gettext_lazy as _
 
 class CustomUserManager(UserManager):
     def get_by_natural_key(self, username):
@@ -13,5 +15,16 @@ class CustomUserManager(UserManager):
 
 # Create your models here.
 class CustomUser(AbstractUser):
+    username_validator = RegexValidator(r'^[0-9a-zA-Z]*$', 'Alphanumeric characters only.')
+    username = models.CharField(
+        _('username'),
+        max_length=150,
+        unique=True,
+        help_text=_('Alphanumeric characters only'),
+        validators=[username_validator],
+        error_messages={
+            'unique': _("A user with that username already exists."),
+        },
+    )
     email = models.EmailField(blank=False, unique=True)
     objects = CustomUserManager()
