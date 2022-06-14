a02aea47e71d804291bda186ad0af392f97ae134
ASC <asc@adamsmith.ie>
2022-06-14 13:26:57 +0100

    frontend build completed, migrated to django app

--- app/app/settings.py
+++ app/app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import environ
+import os
 
 # Initialise environment variables
 env = environ.Env()
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
     'djoser',
     'users',
     'tasks',
+    'frontend',
 ]
 
 MIDDLEWARE = [
@@ -154,6 +156,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'frontend/static')
 STATIC_URL = '/static/'
 
 # Default primary key field type
--- app/app/urls.py
+++ app/app/urls.py
@@ -15,13 +15,16 @@ Including another URLconf
 """
 from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.urls.conf import include
 from users.urls import urlpatterns as users_urlpatterns
+from frontend import views as frontend_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', frontend_views.index),
     path('', include('tasks.urls')),
 ]
 
 urlpatterns += users_urlpatterns
+urlpatterns += [re_path(r'^.*', frontend_views.index)]
