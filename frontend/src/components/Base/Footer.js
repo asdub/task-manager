d474487e231dc63e8fd43e8fbcf7521671304eb6
ASC <asc@adamsmith.ie>
2022-07-19 13:27:45 +0100

    fifix: Added theme toggle to auth view footer

--- frontend/src/components/Base/Footer.js
+++ frontend/src/components/Base/Footer.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import { Box, Link, useTheme } from '@mui/material';
-import GitHubIcon from '@mui/icons-material/GitHub';
+import React from "react";
+import { Box, IconButton, Link, useTheme } from "@mui/material";
+import { Brightness2Outlined, Brightness6Outlined } from "@mui/icons-material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import { ThemeModeContext } from "src/context/ThemeModeProvider";
+import { AuthContext } from "src/context/AuthContextProvider"
 
 export default function Footer() {
+    const themeMode = React.useContext(ThemeModeContext);
+    const { isAuthenticated } = React.useContext(AuthContext);
     const theme = useTheme()
 
     const styles = {
@@ -12,30 +17,44 @@ export default function Footer() {
             width: '100%',
             display: 'flex',
             alignItems: 'center',
-            justifyContent: 'flex-end',
+            justifyContent: 'space-between',
             backgroundColor: theme.palette.secondary.main,
             p:  theme.spacing(1),
+        },
+        div: {
+            display: 'flex',
+            alignItems: 'center',
         },
     }
 
     return (
         <>
             <Box component="footer" sx={styles.footer}>
-                <GitHubIcon color="white" />
-                <Link
-                    component="a"
-                    variant="caption"
-                    target="_blank" 
-                    rel="noopener noreferrer"
-                    href={process.env.REACT_APP_GITHUB_LINK}
-                    key="github"
-                    sx={{ 
-                        p:1,
-                        color: theme.palette.white.main,
-                     }}
-                >
-                    View on Github
-                </Link>
+                <Box sx={styles.div}>
+                    <GitHubIcon color="white" />
+                    <Link
+                        component="a"
+                        variant="caption"
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                        href={process.env.REACT_APP_GITHUB_LINK}
+                        key="github"
+                        sx={{ 
+                            p:1,
+                            color: theme.palette.white.main,
+                        }}
+                    >
+                        View on Github
+                    </Link>
+                </Box>
+                { !isAuthenticated ? (
+                    <IconButton sx={{ ml: 1 }} onClick={themeMode.toggleThemeMode}>
+                    {theme.palette.mode === "dark" ?
+                        <Brightness6Outlined /> :
+                        <Brightness2Outlined color="white" />}
+                    </IconButton>
+                ) : ""
+                }
             </Box>
         </>
     )
